946d8bcbcf56ba3e9dde2f392cb4268b4e0ffd43
Gcolin1 <guilhermecolin1@gmail.com>
2023-03-09 11:56:09 -0300

    tabela enfermeiros

--- src/main/java/med/voll/api/enfermeiro/DadosCadastroEnfermeiros.java
+++ src/main/java/med/voll/api/enfermeiro/DadosCadastroEnfermeiros.java
@@ -1,12 +1,23 @@
 package med.voll.api.enfermeiro;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroEnfermeiros(
+        @NotBlank(message = "nome é obrigatório")
         String nome,
+        @NotBlank(message = "cpf é obrigatório")
+        @Pattern(regexp = "\\d{11}", message = "cpf incorreto")
         String cpf,
+        @NotBlank(message = "email é obrigatório")
+        @Email(message = "email incorreto")
         String email,
+        @NotBlank(message = "cre é obrigatório")
+        @Pattern(regexp = "\\d{4,6}", message = "cre incorreto")
         String cre,
+        @NotBlank(message = "telefone é obrigatório")
         String telefone,
         DadosEndereco endereco
 
